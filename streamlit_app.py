3919194c15e31ddfee82e4b0bc09bf443d6b7a9f
Kendall Sisk <kbeck36369@yahoo.com>
2026-05-27 13:04:56 -0400

    Update streamlit_app.py
    Adding json data inside response object

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,9 +1,10 @@
 # Import python packages
 import streamlit as st
 from snowflake.snowpark.functions import col
+# New section to display smoothiefroot nutrition information
 import requests  
 smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-st.text(smoothiefroot_response)
+st.text(smoothiefroot_response).json()
 
 
 # Write directly to the app
