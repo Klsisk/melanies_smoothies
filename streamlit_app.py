ce25c0e8ab029011b451e0d0eae3937c5bc2c2c9
Kendall Sisk <kbeck36369@yahoo.com>
2026-05-28 13:29:24 -0400

    Update streamlit_app.py
    Adding function into get request, making use of the SEARCH_ON value

--- streamlit_app.py
+++ streamlit_app.py
@@ -43,7 +43,7 @@ if ingredients_list:
 
         st.subheader(fruit_chosen + ' Nutrition Information')
         # 2. Use 'search_on' in the API URL instead of 'fruit_chosen'
-        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/" + search_on)
+        smoothiefroot_response = requests.get(f"https://my.smoothiefroot.com/api/fruit/{search_on}")
         sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
 
     #st.write(ingredients_string)
